fea6de82d8f42358ba0d713e003f15a3e24a4f78
yc <99346683+YChen0317@users.noreply.github.com>
2022-07-31 17:37:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,10 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#password
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+#qianming
+sed -i "s/OpenWrt /阿巴巴巴 $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
